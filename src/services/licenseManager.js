9802c94847c487559b794f61a98b16c09b9e5917
d1scal <evgeniygroza@discal.ru>
2026-05-20 17:14:11 +0300

    use production license api

--- src/services/licenseManager.js
+++ src/services/licenseManager.js
@@ -10,7 +10,7 @@ const execFileAsync = promisify(execFile);
 
 const OFFLINE_GRACE_MS = 14 * 24 * 60 * 60 * 1000;
 const CACHE_FILE = "license.json";
-const DEFAULT_LICENSE_API_URL = "http://127.0.0.1:8787";
+const DEFAULT_LICENSE_API_URL = "https://harmonia-bot-l4ed.onrender.com";
 const DEFAULT_LOCAL_LICENSE_DB_PATH = path.join(os.homedir(), "harmonia-bot", "harmonia-bot.sqlite");
 
 function normalizeLicenseKey(input) {
